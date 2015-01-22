ddad26523c1c54ee2605b2e47b923f98a69ab229
stujo <stujo@users.noreply.github.com>
2015-01-21 22:50:21 -0800

    loopback passport configured without remote providers but I think the framework is in place

--- server/server.js
+++ server/server.js
@@ -1,12 +1,196 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
+
+// Passport configurators..
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+/*
+ * body-parser is a piece of express middleware that
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body`
+ *
+ */
+var bodyParser = require('body-parser');
+
+/**
+ * Flash messages for passport
+ *
+ * Setting the failureFlash option to true instructs Passport to flash an
+ * error message using the message given by the strategy's verify callback,
+ * if any. This is often the best approach, because the verify callback
+ * can make the most accurate determination of why authentication failed.
+ */
+var flash      = require('express-flash');
+
+// Set up the /favicon.ico
+app.use(loopback.favicon());
+
+// request pre-processing middleware
+app.use(loopback.compress());
+
+// -- Add your pre-processing middleware here --
+
+// Setup the view engine (jade)
+var path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// boot scripts mount components like REST API
 boot(app, __dirname);
 
 console.log("config loaded from " + app.get('loaded_from') );
+
+// to support JSON-encoded bodies
+app.use(bodyParser.json());
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// The access token is only available after boot
+app.use(loopback.token({
+  model: app.models.accessToken
+}));
+
+app.use(loopback.cookieParser(app.get('cookieSecret')));
+app.use(loopback.session({
+  secret: 'kitty',
+  saveUninitialized: true,
+  resave: true
+}));
+passportConfigurator.init();
+
+// We need flash messages to see passport errors
+app.use(flash());
+
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: loopbackPassport.UserIdentity,
+  userCredentialModel: loopbackPassport.UserCredential
+});
+
+// attempt to build the providers/passport config
+try {
+  var passportProvidersConfig = require('./passport-providers-config')(app);
+  for (var s in passportProvidersConfig) {
+    var c = passportProvidersConfig[s];
+    c.session = c.session !== false;
+    passportConfigurator.configureProvider(s, c);
+  }
+
+} catch (err) {
+  console.trace(err);
+  process.exit(1); // fatal
+}
+
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+
+app.get('/', function (req, res, next) {
+  res.render('pages/index', {user:
+    req.user,
+    url: req.url
+  });
+});
+
+app.get('/auth/account', ensureLoggedIn('/login.html'), function (req, res, next) {
+  res.render('pages/loginProfiles', {
+    user: req.user,
+    url: req.url
+  });
+});
+
+app.get('/link/account', ensureLoggedIn('/login.html'), function (req, res, next) {
+  res.render('pages/linkedAccounts', {
+    user: req.user,
+    url: req.url
+  });
+});
+
+app.get('/local', function (req, res, next){
+  res.render('pages/local', {
+    user: req.user,
+    url: req.url
+  });
+});
+
+app.get('/signup', function (req, res, next){
+  res.render('pages/signup', {
+    user: req.user,
+    url: req.url
+  });
+});
+
+app.post('/signup', function (req, res, next) {
+
+  var User = app.models.user;
+
+  var newUser = {};
+  newUser.email = req.body.email.toLowerCase();
+  newUser.username = req.body.username.trim();
+  newUser.password = req.body.password;
+
+  User.create(newUser, function (err, user) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('back');
+    } else {
+      // Passport exposes a login() function on req (also aliased as logIn())
+      // that can be used to establish a login session. This function is
+      // primarily used when users sign up, during which req.login() can
+      // be invoked to log in the newly registered user.
+      req.login(user, function (err) {
+        if (err) {
+          req.flash('error', err.message);
+          return res.redirect('back');
+        }
+        return res.redirect('/auth/account');
+      });
+    }
+  });
+});
+
+app.get('/login', function (req, res, next){
+  res.render('pages/login', {
+    user: req.user,
+    url: req.url
+   });
+});
+
+app.get('/link', function (req, res, next){
+  res.render('pages/link', {
+    user: req.user,
+    url: req.url
+  });
+});
+
+app.get('/auth/logout', function (req, res, next) {
+  req.logout();
+  res.redirect('/');
+});
+
+// We already mounted the static files in middleware.json
+
+// -- Mount static files here--
+// All static middleware should be registered at the end, as all requests
+// passing the static middleware are hitting the file system
+// Example:
+// var path = require('path');
+// app.use(loopback.static(path.resolve(__dirname, '../client/public')));
+
+// Requests that get this far won't be handled
+// by any middleware. Convert them into a 404 error
+// that will be handled later down the chain.
+app.use(loopback.urlNotFound());
+
+// The ultimate error handler.
+app.use(loopback.errorHandler());
 
 app.start = function() {
   // start the web server
