c35eee46e42d5d9b1c8e6c3f7b3205da54454142
stujo <stujo@users.noreply.github.com>
2015-01-21 23:02:28 -0800

    Moving dev cookie secret to dev config

--- server/config.development.js
+++ server/config.development.js
@@ -1,5 +1,7 @@
 module.exports = {
   host: "0.0.0.0",
   port: 3000,
-  loaded_from: __filename
+  loaded_from: __filename,
+  cookieSecret: "3eecdea8-d295-42ec-a5cc-ffabc176f727",
+  saveLiveEdit: true
 };
