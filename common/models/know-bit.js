6c492d50136cc99349a251b7e4969b8c4f046810
stujo <stujo@users.noreply.github.com>
2015-01-21 16:56:37 -0800

    Adding http://localhost:3000/api/knowbits/my_remote_method to KnowBit model

--- common/models/know-bit.js
+++ common/models/know-bit.js
@@ -1,3 +1,16 @@
 module.exports = function(KnowBit) {
+    KnowBit.my_remote_method = function(cb) {
+
+    var response = 'This is my_remote_method on the KnowBit model';
+
+    cb(null, response);
+  };
+  KnowBit.remoteMethod(
+    'my_remote_method',
+    {
+      http: {path: '/my_remote_method', verb: 'get'},
+      returns: {arg: 'status', type: 'string'}
+    }
+  );
 
 };
